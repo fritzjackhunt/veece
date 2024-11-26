93805a4f82de258abb666f98441060ead6d12024
Aniekan Essiet <essiet.aniekan@yahoo.com>
2024-11-27 01:27:53 +0300

    update

--- Veece/src/app/pages/verify/verify.page.ts
+++ Veece/src/app/pages/verify/verify.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BvnVerificationService } from '.../bvn-verification.service';
+
 
 @Component({
   selector: 'app-verify',
@@ -7,21 +7,5 @@ import { BvnVerificationService } from '.../bvn-verification.service';
   styleUrls: ['./verify.page.scss'],
 })
 export class VerifyPage {
-
-  bvn : string;
-  verificationResult : any;
-
-  constructor( private bvnService: BvnVerificationService) { }
-
-  verifyBvn () {
-    this.bvnService.verifyBvn(this.bvn).subscribe(
-      (response: { data: any; }) => {
-        this.verificationResult = response.data;
-      },
-      (error: any) => {
-        console.error('Error verifying BVN:', error);
-      }
-    );
-  }
 
 }
